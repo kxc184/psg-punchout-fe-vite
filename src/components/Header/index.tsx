4ae4657dd1c04aebfce1f79b44c23ab155d60038
Kyle Corcoran <Kyle.Corcoran@sherwin.com>
2025-09-08 14:54:53 -0400

    fix: spelling for bannerboundary

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,13 @@
 import { Link } from "react-router";
 // import HeaderMobile from "./HeaderMobile";
 import UtilityNav from "../UtilityNav";
-import BannerBoundry from "../Banner/BannerBoundry";
+import BannerBoundary from "../Banner/BannerBoundary";
 
 const Header = () => {
   return (
     <header>
       <UtilityNav />
-      <BannerBoundry />
+      <BannerBoundary />
       {/* screen sizes below md will show mobile version of header and hide desktop version, vise versa */}
       {/* <HeaderMobile miniCartData={miniCartData} megaMenuData={megaMenuData} /> */}
       <section className="sw:hidden sw:md:block sw:w-full sw:mx-auto">
